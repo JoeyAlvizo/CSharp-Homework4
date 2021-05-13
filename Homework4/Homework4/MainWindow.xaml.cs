1d7897af541cdb3bc9933c253de3c2d69be7b731
JoeyAlvizo <joey@joeyalvizo.com>
2021-05-13 00:10:39 -0500

    finish homework

--- Homework4/Homework4/MainWindow.xaml.cs
+++ Homework4/Homework4/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,17 @@ namespace Homework4
         public MainWindow()
         {
             InitializeComponent();
+            uxSubmit.IsEnabled = false;
+        }
+        private void keyUpEvent(object sender, KeyEventArgs e)
+        {
+            String uSPattern = @"^\d{5}(?:-?\d{4})?$";
+            String canadaPattern = @"^([ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ])\ {0,1}(\d[ABCEGHJKLMNPRSTVWXYZ]\d)$";
+            Regex uSRx = new Regex(uSPattern);
+            Regex canadaRx = new Regex(canadaPattern);
+            bool uS = uSRx.IsMatch(uxZipCode.Text);
+            bool canada = canadaRx.IsMatch(uxZipCode.Text);
+            uxSubmit.IsEnabled = uS || canada;
         }
     }
 }
